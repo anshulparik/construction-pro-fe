712badaf2a328f2f9a5da2e48d97954c569ae393
Anshul Parik <anshulparik@gmail.com>
2024-11-09 23:14:26 +0530

    chore:angular starter code setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LocationPageComponent } from './components/location-page/location-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, LocationPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
